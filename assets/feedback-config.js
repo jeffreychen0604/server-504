e858b3fbc91256497c2ef8f4fefde3e912d07d9d
jeffreychen0604 <duong1856@gmail.com>
2026-08-07 19:30:58 +0700

    feat: enable anonymous feedback API

--- assets/feedback-config.js
+++ assets/feedback-config.js
@@ -1,2 +1,2 @@
 window.SERVER504_CONFIG = window.SERVER504_CONFIG || {};
-window.SERVER504_CONFIG.feedbackApi = '';
+window.SERVER504_CONFIG.feedbackApi = 'https://server-504-feedback.duong1856.workers.dev';
